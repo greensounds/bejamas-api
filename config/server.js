1b391d0ae662de7984579b4f5d848138f0461724
Jesus Diaz <jeda.1992@hotmail.com>
2021-05-11 18:10:50 -0500

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15090ba9ed38d1535952b69c1a0d1576'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6cc23472-9601-4ad7-909c-2f5fefb563ac'
+};
